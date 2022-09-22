c3fa813cf3771de7dffe83e6e84107d01651ab27
AsmaBTC <asmabaitiche2001@gmail.com>
2022-09-22 15:28:21 +0200

    ajout classe DBParams et initialisation attributs dans Main

--- Projet_BDDA_BAITICHE_AHMEDZAID_BOUAKI_DERAI/CODE/src/Main.java
+++ Projet_BDDA_BAITICHE_AHMEDZAID_BOUAKI_DERAI/CODE/src/Main.java
@@ -1,8 +1,11 @@
+import java.io.File;
 
 public class Main {
 	public static void main(String[] args ) {
         System.out.println("Hello world");
-    
+        DBParams.DBPath= new File("/~/BDDA/Projet_BDDA_BAITICHE_AHMEDZAID_BOUAKI_DERAI/DB");
+        DBParams.pageSize=4096;
+        DBParams.maxPagesPerFile=4;
     }
 
 }
