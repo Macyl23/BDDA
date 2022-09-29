0653b9c2c63e40c1ce08913d223e9032f2011108
AsmaBTC <asmabaitiche2001@gmail.com>
2022-09-29 23:31:00 +0200

    ajout des methodes BufferManager

--- Projet_BDDA_BAITICHE_AHMEDZAID_BOUAKI_DERAI/CODE/src/BufferManager.java
+++ Projet_BDDA_BAITICHE_AHMEDZAID_BOUAKI_DERAI/CODE/src/BufferManager.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.nio.ByteBuffer;
 
 public class BufferManager{
     static BufferManager leBufferManager=new BufferManager();
@@ -7,6 +8,18 @@ public class BufferManager{
     public static BufferManager getLeBufferManager(){
     return leBufferManager;
 }
+//retourner un buffer associe a une frame 
+public ByteBuffer GetPage(PageId pageId){
+    return null;}
+
+//actualisation du flag dirty, decrementation du pin_count
+public void FreePage(PageId pageId, boolean valdirty){}
+
+/*ecriture des pages modifiees sur le disque 
+remise a 0 des flags/ informartions et contenus des buffers
+*/
+public void FlushhAll(){}
+
 
 
 }
