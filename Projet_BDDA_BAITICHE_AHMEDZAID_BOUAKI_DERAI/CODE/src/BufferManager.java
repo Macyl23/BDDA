2ee0f74ef32883481d8c2c302272412a1bc19a89
AsmaBTC <asmabaitiche2001@gmail.com>
2022-10-05 14:53:29 +0200

    methode getPage et classe ByteBuffer DONE!

--- Projet_BDDA_BAITICHE_AHMEDZAID_BOUAKI_DERAI/CODE/src/BufferManager.java
+++ Projet_BDDA_BAITICHE_AHMEDZAID_BOUAKI_DERAI/CODE/src/BufferManager.java
@@ -1,12 +1,15 @@
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.ByteBuffer;
-
+import java.util.ArrayList;
 
 public class BufferManager {
     private static BufferManager leBufferManager = new BufferManager();
     private Frame[] buffPool;
     private DiskManager discManager;
+    ArrayList<Frame> lru= new ArrayList<Frame>();
+    Frame pRemplacee=null;
+
 
     private BufferManager() {
         this.buffPool = new Frame[DBParams.frameCount];
@@ -17,7 +20,14 @@ public class BufferManager {
         return leBufferManager;
     }
 
-    // retourner un buffer associe a une frame
+    /*
+     * @param pageId
+     * methode qui permet de chercher une page dans le bufferPool
+     * cete methode implemente la politique de LRU: least recently used 
+     * on cherche dans le buffer si la page deja existe 
+     * si non on cherche une case vide 
+     * si non on remplace en suivant la politique LRU
+     */
     public ByteBuffer getPage(PageId pageId) throws IOException {
         // verifier si pageId se trouve deja dans le buffPool
         for (int i = 0; i < buffPool.length; i++) {
@@ -31,11 +41,24 @@ public class BufferManager {
                     buffPool[i].setFlagDirty(false);
                     discManager.readPage(pageId, buffPool[i].getBuff());
                 } else {
+               
+                    if(buffPool[i].getPinCount()==0){
+                        lru.add(buffPool[i]);
+                    }
+                    int min=lru.get(0).getTs();
+                    for(int j=0; j<lru.size(); j++){
+                        if(min>lru.get(j).getTs()){
+                        min=lru.get(j).getTs();
+                        pRemplacee.setBuff(lru.get(j).getBuff());
+                        }
+                    }
+                        freePage(pRemplacee.getPageId(), pRemplacee.getFlagDirty());
+                }
 
                 }
 
             }
-        }
+        
         return null;
     }
 
@@ -66,12 +89,12 @@ public class BufferManager {
      * ecriture des pages modifiees sur le disque
      * remise a 0 des flags/ informartions et contenus des buffers
      */
-    public void flushhAll()throws FileNotFoundException, IOException {
+    public void flushAll() throws FileNotFoundException, IOException {
         for(Frame frame: this.buffPool){
             if(frame.getFlagDirty()){
                 this.discManager.writePage(frame.getPageId(), frame.getBuff());
             }
-            frame.setPageId(new pageId(-1,0));
+            frame.setPageId(new PageId(-1,0));
             frame.setPinCount(0);
             frame.setFlagDirty(false);    
         }
@@ -79,3 +102,4 @@ public class BufferManager {
     }
 
 }
+
