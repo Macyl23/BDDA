dc88616a671a0f36977067cb794725ef75a55900
Arthur bouaki <204bouaki.arthur@gmail.com>
2022-10-13 13:12:57 +0200

    modification de la methode String et commentaire des classe colInfo RelationInfo et Catalog

--- Projet_BDDA_BAITICHE_AHMEDZAID_BOUAKI_DERAI/CODE/src/colInfo.java
+++ Projet_BDDA_BAITICHE_AHMEDZAID_BOUAKI_DERAI/CODE/src/colInfo.java
@@ -1,34 +1,49 @@
 public class ColInfo {
     // tout les instances de cette classe sont des chaines pour une meilleur gestion
     // indication de la prof
+    //  mettre dans cette classe le nom et le type de la colonne
+    
+  // ATTRIBUTS NOM COLONNES ET TYPES COLONNES
+
     private String nom;
     private String type;
-
+/* constructeur colInfo*/
     public ColInfo(String nom, String type) {
         this.nom = nom;
         this.type = type;
 
     }
+    /* methode init() jsais pas si c'est necessaire je laisse au cas ou faudra implementer*/
 
     public void init() {
     };
 
+    /* methode finish jsais pas si c'est necessaire je laisse au cas ou faudra implementer*/
+
     public void finish() {
     };
+
+        /* methode getNom() pour obtenir nom colonne*/
 
     public String getNom() {
         return this.nom;
     }
 
+            /* methode getType () pour obtenir type de colonnes*/
+
     public String getType() {
         return this.type;
     }
+        /* methode setNom () pour obtenir set nom  colonne*/
 
     public void setNom(String nom) {
         this.nom = nom;
     }
 
-    public String setTypeReal(String typeReal) {
+        /* methode setNom () pour obtenir set nom  colonne*/
+
+
+    public String setType(String typeReal) {
         return typeReal;
     }
 
