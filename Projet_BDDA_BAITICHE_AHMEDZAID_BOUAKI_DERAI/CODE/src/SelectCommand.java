d09b8bc5ffb15ca4f4d71b1c2e8dd984fee190a0
Macyl23 <ahmedzaidmacyl99@gmail.com>
2022-11-24 12:51:49 +0100

    MISE a jour SelectCondition

--- Projet_BDDA_BAITICHE_AHMEDZAID_BOUAKI_DERAI/CODE/src/SelectCommand.java
+++ Projet_BDDA_BAITICHE_AHMEDZAID_BOUAKI_DERAI/CODE/src/SelectCommand.java
@@ -1,4 +1,6 @@
 import java.io.IOException;
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
 
 
@@ -21,10 +23,8 @@ public class SelectCommand{
     public  void execute() throws IOException{
         if(commande.length > 5){
             /* Des critères ont été entrées  */
-            ArrayList<String> 
-            for(int i=0; i<commande[5].length(); i++){
-
-            }
+            ArrayList<SelectCondition> criteres = listeCriteres();
+            
         }else{
             records=FileManager.leFileManager.getAllRecords(Catalog.leCatalog.getRelationInfo(nomRelation));
             afficherRecords();
@@ -37,6 +37,70 @@ public class SelectCommand{
         }
     }
 
-
     
+    private ArrayList<SelectCondition> listeCriteres(){
+        String[] criteres = commande[5].split("AND");
+            ArrayList<SelectCondition>  sc = new ArrayList<>();
+            for(int i=0 ; i< criteres.length ; i++){
+                sc.add(parseCmd(criteres[i]));
+            }
+            return sc;
+    }
+    private SelectCondition parseCmd(String str){
+        int indice=0; 
+        String op="";
+        String val="";
+        for(int i=0 ; i< SelectCondition.getOperateur().length;i++){
+            if(str.contains(SelectCondition.getOperateur()[i])){
+                String[] condition = str.split(SelectCondition.getOperateur()[i]);
+                indice = getIndexColumn(nomRelation, condition[0]); 
+                op = SelectCondition.getOperateur()[i];
+                val = condition[1];
+            }
+        }
+        return new SelectCondition(indice,op , val);
+    }
+
+    private int getIndexColumn(String str,String nomColonne){
+        RelationInfo r = Catalog.leCatalog.getRelationInfo(str);
+        for(int i=0 ; i<r.getNbColonnes() ; i++){
+            if(r.getInfoColonne().get(i).getNom().equals(nomColonne)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private boolean testCondition(SelectCondition sc,String valeurRecord){
+        RelationInfo r = Catalog.leCatalog.getRelationInfo(nomRelation);
+        String type = r.getInfoColonne().get(sc.getIndiceColonne()).getType();
+
+        switch(type){
+            case "INTEGER":
+                return sc.verifConditionNombre(Float.parseFloat(valeurRecord), Float.parseFloat(sc.getValeurComparaison()),sc.getOp());
+            case "REAL":
+                return sc.verifConditionNombre(Float.parseFloat(valeurRecord), Float.parseFloat(sc.getValeurComparaison()),sc.getOp());
+            default:
+                return sc.verifConditionString(valeurRecord, valeurRecord, type);
+        }
+
+    }
+    
+    // private ArrayList<Record> getFilteredRecordsInDataPage(RelationInfo r,ArrayList<SelectCondition> sc) throws IOException{
+    //     ArrayList<PageId> allDataPage = FileManager.leFileManager.getAllDataPage(r);
+    //     ArrayList<Record> records = new ArrayList<>();
+    //     for(int i=0 ; i<allDataPage.size() ; i++){
+    //         ByteBuffer bufferDataPage = BufferManager.leBufferManager.getPage(allDataPage.get(i));
+    //         int nbSlots = bufferDataPage.getInt(DBParams.pageSize-8);
+    //         for(int j=0 ; j<nbSlots ; j++){
+    //             for(int k=0 ; k<sc.size() ; k++){
+    //                 int posDebutValeur = bufferDataPage.getInt(4*sc.get(k).getIndiceColonne());
+    //                 int valeur = 
+    //                 if(testCondition(sc.get(k), nomRelation)){
+    //                     records.add(null)
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
 }
--- Projet_BDDA_BAITICHE_AHMEDZAID_BOUAKI_DERAI/CODE/src/SelectCondition.java
+++ Projet_BDDA_BAITICHE_AHMEDZAID_BOUAKI_DERAI/CODE/src/SelectCondition.java
@@ -1,12 +1,84 @@
 public class SelectCondition {
-    int indiceColonne;
-    String operateur;
-    int valeurComparaison;
+    private int indiceColonne;
+    public int getIndiceColonne() {
+        return indiceColonne;
+    }
 
-    public SelectCondition(int iColonne, String operateur, int valeurComparaison) {
-        this.indiceColonne = iColonne;
-        this.operateur = operateur;
+    public void setIndiceColonne(int indiceColonne) {
+        this.indiceColonne = indiceColonne;
+    }
+
+    private static final String[] operateur = {"=","<",">","<>","<=",">="};
+    public static String[] getOperateur() {
+        return operateur;
+    }
+
+    private String valeurComparaison;
+    public String getValeurComparaison() {
+        return valeurComparaison;
+    }
+
+    public void setValeurComparaison(String valeurComparaison) {
         this.valeurComparaison = valeurComparaison;
     }
 
+    private String op;
+
+    public String getOp() {
+        return op;
+    }
+
+    public void setOp(String op) {
+        this.op = op;
+    }
+
+    public SelectCondition(int iColonne,String op, String valeurComparaison) {
+        this.indiceColonne = iColonne;
+        this.valeurComparaison = valeurComparaison;
+        this.op=op;
+    }
+
+    public boolean verifConditionNombre(Float valeurRecord, Float valeurCondition,String op){
+        switch (op){
+            case "=":
+			return valeurCondition == valeurRecord;
+            case "<":
+                return valeurCondition < valeurRecord;
+            case ">":
+                return valeurCondition > valeurRecord;
+            case "<=":
+                return valeurCondition <= valeurRecord;
+            case ">=":
+                return valeurCondition >= valeurRecord;
+            case "<>":
+                return valeurCondition != valeurRecord;
+            default:
+                System.out.println("Operateur incorrect");
+                return false;
+        }
+    }
+
+    public boolean verifConditionString(String valeurRecord, String valeurCondition,String op){
+        switch (op){
+            case "=":
+			return valeurCondition.equals(valeurRecord);
+            case "<":
+                return valeurCondition.compareTo(valeurRecord) < 0;
+            case ">":
+                return valeurCondition.compareTo(valeurRecord) > 0;
+            case "<=":
+                return valeurCondition.compareTo(valeurRecord) <= 0;
+            case ">=":
+                return valeurCondition.compareTo(valeurRecord) >= 0;
+            case "<>":
+                return valeurCondition.compareTo(valeurRecord) != 0;
+            default:
+                System.out.println("Operateur incorrect");
+                return false;
+        }
+    }
 }
+    
+
+    
+
